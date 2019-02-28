cd061d7b2a864e09df8873b456697f175980ccf3
Tim Buckland <tim.buckland@totaljobsgroup.com>
2019-02-28 17:04:35 +0000

    increase size of filename

--- dash/results/models.py
+++ dash/results/models.py
@@ -20,7 +20,7 @@ class Artefact(models.Model):
     test_name = models.CharField(max_length=200)
     app_name = models.CharField(max_length=50)
     run_name = models.CharField(max_length=20)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=160)
     desc = models.CharField(max_length=200)
     document = models.FileField(upload_to='artefacts/%Y/%m/%d/', default=None)
     date_created = models.DateTimeField(auto_now_add=True)
